91c1de4af27cf4c3ff70f142b6e178de789030fc
sserva1 <77353742+sserva1@users.noreply.github.com>
2021-03-30 10:55:46 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -22,7 +22,8 @@ $.getJSON(weatherAlertsUrl, function(data) {
     style: function(feature){
       var alertColor = 'orange';
       if (feature.properties.severity === 'Severe') alertColor = 'red';
-      if (feature.properties.severity === 'Minor') alertColor = 'Purple';
+      if (feature.properties.severity === 'Extreme') alertColor = 'purple';
+      if (feature.properties.severity === 'Minor') alertColor = 'Green';
       return { color: alertColor }
     },
     // Add a popup on each feature showing the NWS alert headline
